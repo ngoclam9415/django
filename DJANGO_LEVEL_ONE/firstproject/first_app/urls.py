008779bf3a8316bc7bc06ca17921e6180aa4ff09
Nguyen Ngoc Lam <lam@Lams-MacBook-Pro.local>
2019-10-11 17:54:42 +0700

    Add Url Explanation ( Self explaination )

--- DJANGO_LEVEL_ONE/firstproject/first_app/urls.py
+++ DJANGO_LEVEL_ONE/firstproject/first_app/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 from first_app import views
 
 urlpatterns = [
-    url('', views.first_app_index, name="index"),
-    url('/hehe/', views.hehe, name="hehe")
+    url('aloha', views.first_app_index, name="first_app_index"),
+    url('hehe', views.hehe, name="hehe"),
+    url('', views.index, name="index")
+    # When getin 127.0.0.1/first_app we get inside this. Then Django search for each url in urlpatterns in orders. 
+    # If we put '' at first place, for any url with /first_app/... , it will match with '' first then the others /first_app/hehe , ... will have no effect
+    # URL format is : /first_app/ + 'hehe' or 'aloha' or ''
 ]
